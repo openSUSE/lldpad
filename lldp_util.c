intel/openlldp

commit d43abb0267f36ca0d850bcf85c69fe611b44a96a

lldpad: do not use macv[tap/lan] interfaces as ports
At startup lldpad walks through all network interfaces in the system
and creates internal data structures for them. Some interfaces as
e.g. vlan and wlan are skipped in the walkthrough, some have to be
treated special (e.g. bond devices).

This patch adds macvtap and macvlan interfaces to the list of devices
that are skipped as we do not want to send out EVB/ECP frames on them.

Signed-off-by: Jens Osterkamp <jens at linux.vnet.ibm.com>
Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

lldp_util.c
@@ -36,6 +36,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <net/if_arp.h>
+#include <netlink/msg.h>
 #include <arpa/inet.h>
 #include <linux/if.h>
 #include <linux/if_bonding.h>
@@ -44,6 +45,7 @@
 #include <linux/wireless.h>
 #include <linux/sockios.h>
 #include <linux/ethtool.h>
+#include <linux/rtnetlink.h>
 #include <dirent.h>
 #include "lldp.h"
 #include "lldp_util.h"
@@ -139,6 +141,8 @@ int is_valid_lldp_device(const char *device_name)
 	if (is_vlan(device_name))
 		return 0;
 	if (is_bridge(device_name))
+		return 0;
+	if (is_macvtap(device_name))
 		return 0;
 	return 1;
 }
@@ -615,6 +619,92 @@ int is_wlan(const char *ifname)
 		perror("is_wlan() open socket error");
 	}
 	return rc;
+}
+
+#define NLMSG_SIZE 1024
+
+static struct nla_policy ifla_info_policy[IFLA_INFO_MAX + 1] =
+{
+  [IFLA_INFO_KIND]       = { .type = NLA_STRING},
+  [IFLA_INFO_DATA]       = { .type = NLA_NESTED },
+};
+
+int is_macvtap(const char *ifname)
+{
+	int ret, s;
+	struct nlmsghdr *nlh;
+	struct ifinfomsg *ifinfo;
+	struct nlattr *tb[IFLA_MAX+1],
+		      *tb2[IFLA_INFO_MAX+1];
+
+	s = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE);
+
+	if (s < 0) {
+		goto out;
+	}
+
+	nlh = malloc(NLMSG_SIZE);
+	memset(nlh, 0, NLMSG_SIZE);
+
+	if (!nlh) {
+		goto out;
+	}
+
+	nlh->nlmsg_len = NLMSG_LENGTH(sizeof(struct ifinfomsg));
+        nlh->nlmsg_type = RTM_GETLINK;
+        nlh->nlmsg_flags = NLM_F_REQUEST;
+
+	ifinfo = NLMSG_DATA(nlh);
+	ifinfo->ifi_family = AF_UNSPEC;
+	ifinfo->ifi_index = get_ifidx(ifname);
+
+	ret = send(s, nlh, nlh->nlmsg_len, 0);
+
+	if (ret < 0) {
+		goto out_free;
+	}
+
+	memset(nlh, 0, NLMSG_SIZE);
+
+	do {
+		ret = recv(s, (void *) nlh, NLMSG_SIZE, MSG_DONTWAIT);
+	} while ((ret < 0) && errno == EINTR);
+
+	if (nlmsg_parse(nlh, sizeof(struct ifinfomsg),
+			(struct nlattr **)&tb, IFLA_MAX, NULL)) {
+		goto out_free;
+	}
+
+	if (tb[IFLA_IFNAME]) {
+		ifname = (char *)RTA_DATA(tb[IFLA_IFNAME]);
+	} else {
+		ifinfo = (struct ifinfomsg *)NLMSG_DATA(nlh);
+	}
+
+	if (tb[IFLA_LINKINFO]) {
+		if (nla_parse_nested(tb2, IFLA_INFO_MAX, tb[IFLA_LINKINFO],
+				     ifla_info_policy)) {
+			goto out_free;
+		}
+
+		if (tb2[IFLA_INFO_KIND]) {
+			char *kind = (char*)(RTA_DATA(tb2[IFLA_INFO_KIND]));
+			if (!(strcmp("macvtap", kind) && strcmp("macvlan", kind))) {
+				free(nlh);
+				close(s);
+				return true;
+			}
+		}
+
+	} else {
+		goto out_free;
+	}
+
+out_free:
+	free(nlh);
+out:
+	close(s);
+	return false;
 }
 
 int is_router(const char *ifname)

config.c
@@ -367,6 +367,8 @@ void init_ports(void)
 					p->if_name);
 		} else if (is_vlan(p->if_name)) {
 			;
+		} else if (is_macvtap(p->if_name)) {
+			;
 		} else if (is_bridge(p->if_name)) {
 			; /* ignore bridge device */
 		} else if (check_link_status(p->if_name)) {

ecp/ecp_rx.c
@@ -36,6 +36,8 @@
 #include "lldp_mand.h"
 #include "lldp_vdp.h"
 
+void ecp_rx_run_sm(struct vdp_data *);
+
 /* ecp_rx_Initialize - initializes the ecp rx state machine
  * @vd: vd for the state machine
  *

include/lldp_util.h
@@ -48,6 +48,7 @@ int is_bridge(const char *ifname);
 int is_vlan(const char *ifname);
 int is_vlan_capable(const char *ifname);
 int is_wlan(const char *ifname);
+int is_macvtap(const char *ifname);
 int is_valid_mac(const u8 *mac);
 int is_san_mac(u8 *addr);
 int is_ether(const char *ifname);

openSUSE/lldpad

commit ad0aaf147f63a5ff585c8bfbebe77871cf12e0e6

fix lldpad netlink heap access overrun
Fixes: d43abb0267f36 ("lldpad: do not use macv[tap/lan] interfaces as ports")
Signed-off-by: Chunjie Zhu <chunjie.zhu@cloud.com>
Signed-off-by: Aaron Conole <aconole@redhat.com>

lldp_util.c
@@ -672,8 +672,9 @@ static struct nla_policy ifla_info_policy[IFLA_INFO_MAX + 1] =
 
 int is_macvtap(const char *ifname)
 {
-	int ret, s;
+	int ret, s, realsize;
 	struct nlmsghdr *nlh;
+	void *temp;
 	struct ifinfomsg *ifinfo;
 	struct nlattr *tb[IFLA_MAX+1],
 		      *tb2[IFLA_INFO_MAX+1];
@@ -684,13 +685,11 @@ int is_macvtap(const char *ifname)
 		return false;
 	}
 
-	nlh = malloc(NLMSG_SIZE);
+	nlh = calloc(1, NLMSG_SIZE);
 
 	if (!nlh) {
 		goto out;
 	}
-
-	memset(nlh, 0, NLMSG_SIZE);
 
 	nlh->nlmsg_len = NLMSG_LENGTH(sizeof(struct ifinfomsg));
         nlh->nlmsg_type = RTM_GETLINK;
@@ -706,10 +705,23 @@ int is_macvtap(const char *ifname)
 		goto out_free;
 	}
 
-	memset(nlh, 0, NLMSG_SIZE);
+	do {
+		realsize = recv(s, NULL, 0, MSG_DONTWAIT | MSG_PEEK | MSG_TRUNC);
+	} while ((realsize < 0) && errno == EINTR);
+
+	if (realsize < 0) {
+		goto out_free;
+	}
+
+	temp = realloc(nlh, realsize);
+	if (!temp) {
+		goto out_free;
+	}
+	memset(temp, 0, realsize);
+	nlh = temp;
 
 	do {
-		ret = recv(s, (void *) nlh, NLMSG_SIZE, MSG_DONTWAIT);
+		ret = recv(s, (void *) nlh, realsize, MSG_DONTWAIT);
 	} while ((ret < 0) && errno == EINTR);
 
 	if (nlmsg_parse(nlh, sizeof(struct ifinfomsg),